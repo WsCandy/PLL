3134bd216ab4186ea094fa9f30e7c4c6be53940a
Samuel - Work <s-bridge@live.co.uk>
2014-06-02 11:20:55 +0100

    [JS] - Image offset additions

--- assets/js/PLL.js
+++ assets/js/PLL.js
@@ -7,6 +7,12 @@
 		var elements = document.getElementsByClassName('pll');
 		var instance = this;
 
+		var settings = {
+
+			tolerance : 200
+
+		}
+
 		instance.core = {
 
 			init: function() {
@@ -14,34 +20,55 @@
 				for(var i = elements.length -1; i >= 0; i--) {
 					
 					var currentElement = elements[i];
+					
+					if (!instance.core['isVisible'](currentElement)) {
+
+						continue;
+
+					}
 
 					var imgSrc = currentElement.getAttribute('data-src');
 					var imgAlt = currentElement.getAttribute('data-alt');
 
-					instance.core.replaceElement(elements[i], {
+					instance.core['replaceElement'](elements[i], {
 					
 						src : imgSrc, 
-						alt: imgAlt
+						alt : imgAlt
 
 					});
+
 				}
 
 			},
 
 			replaceElement : function(element, data) {
-				
+
 				var parent = element.parentNode;
 				var img = document.createElement('img');
-					img.setAttribute('src', data.src);
+					img.setAttribute('src', data['src']);
 					
-				data.alt ? img.setAttribute('alt', data.alt) : '';
+				data.alt ? img.setAttribute('alt', data['alt']) : '';
 				parent.replaceChild(img, element);
+
+			},
+
+			isVisible : function(element) {
+
+				if(element.offsetTop - settings.tolerance < document.documentElement.clientHeight) {
+
+					return true;
+
+				} else {
+
+					return false;
+
+				}
 
 			}
 
 		}
 
-		instance.core.init();
+		instance.core['init']();
 
 	}
 
