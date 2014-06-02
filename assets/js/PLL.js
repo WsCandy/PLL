0cb8d0df3c11c366d6108d0f17ab8de140ed11cf
Samuel - Work <s-bridge@live.co.uk>
2014-06-02 10:04:36 +0100

    [JS] - Set up image replace methods

--- assets/js/PLL.js
+++ assets/js/PLL.js
@@ -1,5 +1,56 @@
-;(function() {
+;(function(window, document) {
 
 	'use strict';
 
-})();
+	function PLL() {
+
+		var elements = document.getElementsByClassName('pll');
+		var instance = this;
+
+		instance.core = {
+
+			init: function() {
+
+				for(var i = elements.length -1; i >= 0; i--) {
+					
+					var currentElement = elements[i];
+
+					var imgSrc = currentElement.getAttribute('data-src');
+					var imgAlt = currentElement.getAttribute('data-alt');
+
+					instance.core.replaceElement(elements[i], {
+					
+						src : imgSrc, 
+						alt: imgAlt
+
+					});
+				}
+
+			},
+
+			replaceElement : function(element, data) {
+				
+				var parent = element.parentNode;
+				var img = document.createElement('img');
+					img.setAttribute('src', data.src);
+					
+				data.alt ? img.setAttribute('alt', data.alt) : '';
+				parent.replaceChild(img, element);
+
+			}
+
+		}
+
+		instance.core.init();
+
+	}
+
+	window.PLL = PLL;
+
+})(window, document);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	new PLL();
+
+});
